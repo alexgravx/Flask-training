964dea2010e6eddc12ad75cfd29273652c473cd7
Gravereaux Alexandre <alexandre.gravereaux@student-cs.fr>
2024-03-26 01:44:15 +0100

    v3.2 update

--- fbapp/views.py
+++ fbapp/views.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 
-from .utils import find_content
+from .utils import find_content, OpenGraphImage
 
 @app.route('/')
 @app.route('/index/')
@@ -18,12 +18,10 @@ def index():
     else:
         og_url = url_for('index', _external=True)
         og_image = url_for('static', filename='tmp/sample.jpg', _external=True)
-    description = """
-        Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
-    """
+    description = """Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)"""
     page_title = "Le test ultime"
-
     og_description = "Découvre qui tu es vraiment en faisant le test ultime !"
+    
     return render_template('index.html',
                           user_name='Julio',
                           user_image=url_for('static', filename='img/profile.png'),
@@ -41,7 +39,7 @@ def result():
     uid = request.args.get('id')
     profile_pic = 'http://graph.facebook.com/' + uid + '/picture?type=large'
     description = find_content(gender).description
-    img = 'tmp/sample.jpg'
+    img = OpenGraphImage(uid, user_name, description).location
     og_url = url_for('index', img=img, _external=True)
     return render_template('result.html',
                             user_name=user_name,
